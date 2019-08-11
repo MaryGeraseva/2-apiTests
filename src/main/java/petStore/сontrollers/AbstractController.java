a9937a3d5fc2afd88d8ac916f060f4e667dfcb2f
mary <mary.geraseva@gmail.com>
2019-08-11 21:51:23 +0300

    clean

--- src/main/java/petStore/сontrollers/AbstractController.java
+++ src/main/java/petStore/сontrollers/AbstractController.java
@@ -23,7 +23,7 @@ public abstract class AbstractController {
                 .log(LogDetail.ALL);
 
         RestAssured.defaultParser = Parser.JSON;
-        RestAssured.proxy("localhost" , 8888);
+        //RestAssured.proxy("localhost" , 8888);
 
 //        RestAssured.responseSpecification = new ResponseSpecBuilder().log(LogDetail.ALL).build();
     }
